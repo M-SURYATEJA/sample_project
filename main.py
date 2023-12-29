3e9f9df58e0d250af93b42f59d37323f589108c8
HEMANTH SAI SURYA TEJA MUDUNURI <98734860+M-SURYATEJA@users.noreply.github.com>
2023-12-29 10:42:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.write("<h1>hello world</h1>",unsafe_allow_html=True)
 st.write("<h1 style='color:blue;'>HELLO</h1>",unsafe_allow_html=True)
 st.file_uploader("upload file")
-st.image(https://images.wallpapersden.com/image/download/batman-cool-the-dark-knight_bmZpaWyUmZqaraWkpJRqbWdprWhna2k.jpg)
-st.video(https://www.youtube.com/watch?v=099vtSGsLII)
+st.image("https://images.wallpapersden.com/image/download/batman-cool-the-dark-knight_bmZpaWyUmZqaraWkpJRqbWdprWhna2k.jpg")
+st.video("https://www.youtube.com/watch?v=099vtSGsLII")
