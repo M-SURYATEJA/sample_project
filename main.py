a3ae194a27f09f4b4d23485bd48558496d35a30b
HEMANTH SAI SURYA TEJA MUDUNURI <98734860+M-SURYATEJA@users.noreply.github.com>
2023-12-29 10:12:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+st.write("<h1>hello world</h1>",unsafe_allow_html=True)
+st.write("<h1 style='color:blue;'>HELLO</h1>"unsafe_allow_html=True)
+st.file_uploader{"upload file")
+st.image(https://i.pinimg.com/originals/8e/5c/56/8e5c56b66a2c216066033513298d9c99.png)
+st.video(https://www.youtube.com/watch?v=ZcIQ7xkFxd8)
